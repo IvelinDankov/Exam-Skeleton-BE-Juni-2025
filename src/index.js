ed6190f8ceb0c4185fdf602b76dc7cf7c4d50a40
Velin Dankov <velin80@web.de>
2025-06-12 18:01:42 +0300

    add express-handlebars for view rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import express from "express";
+import express from "
+import handlebars from "express-handlebars";
 
 const port = "5000";
 
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 
 app.use(routes);
 
