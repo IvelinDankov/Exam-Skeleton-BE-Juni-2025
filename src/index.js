265c18ce3826001a458db8b719f2c892e1b53242
Velin Dankov <velin80@web.de>
2025-06-13 09:54:53 +0300

    added static filtes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import express from "
-import handlebars from "express-handlebars";
+import express from "express";
 
-const port = "5000";
+const port = 3000;
 
 const app = express();
 
-app.engine('hbs', handlebars.engine({
-  extname: 'hbs'
-}))
-
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
-
-
-app.use(routes);
-
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
